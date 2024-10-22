8cc290abffa38e24f962fd429ec8b678a3a63b56
siham oubalaout <oubalaoutsiham2002@gmail.com>
2024-10-22 20:56:36 +0100

    2

--- SpringDataRest-master/src/main/java/com/ensias/springdatarest/modele/ProprietaireRepo.java
+++
@@ -1,8 +0,0 @@
-package com.ensias.springdatarest.modele;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProprietaireRepo extends CrudRepository<Proprietaire, Long> {
-}
-
